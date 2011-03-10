c48452b989195f7ff0294da66c76caeeb9d89861
Jeff Rafter <jeff@socialrange.org>
2011-03-10 00:41:08 -0800

    Intial working prototype

--- src/models/category.js
+++
@@ -1,9 +0,0 @@
-Questions.Category = Backbone.Model.extend({
-  localStorage: new Store("categories")
-});
-
-Questions.Collections.Categories = Backbone.Collection.extend({
-  model: Questions.Category,
-  localStorage: new Store("categories")
-});
-
